8738c3ca6cbdf5689c330a5b5ee25bcef49db889
Norbert Dziwinski <norbert.dziwinski@mayeryn.com>
2020-09-15 10:26:18 +0200

    initial commit

---
+++ src/main/java/pw/ddteam/webshop/WebShopApplication.java
@@ -0,0 +1,13 @@
+package pw.ddteam.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopApplication.class, args);
+	}
+
+}
