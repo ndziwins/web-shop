b6c1209e374c01f46a5fa1da6ba3d5e481d0a93a
Norbert Dziwinski <norbert.dziwinski@mayeryn.com>
2020-09-16 08:59:37 +0200

    add db migrations

---
+++ src/main/java/pw/ddteam/webshop/model/Product.java
@@ -0,0 +1,52 @@
+package pw.ddteam.webshop.model;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @NotBlank(message = "Product name must not be empty")
+    private String name;
+    private String description;
+    private String picturePath;
+
+    Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPicturePath() {
+        return picturePath;
+    }
+
+    public void setPicturePath(String picturePath) {
+        this.picturePath = picturePath;
+    }
+}
